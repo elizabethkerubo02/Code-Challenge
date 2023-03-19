9322e9971b551bf7241e6f1c35d7a83abaf14a6e
elizabethkerubo02 <keruboelizabeth02@gmail.com>
2023-03-19 22:13:43 +0300

    wrote Moringa School title correctly

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function calculateNetSalary(){
   const benefits = parseInt(document.getElementById("benefits").value);
   const nhifRates = [0,150,300,500,600,750,850,900,950,1000,1100,1200,1300,1400,1500];
   const taxBrackets = [{upperLimit:12298,rate:0},{upperLimit:23885,rate:0.1},{upperLimit:35472,rate:0.15},{upperLimit:47059,rate:0.20},{upperLimit:Infinity,rate:0.25}];
-  const nhif = nhifRates[Math.ceil(basicSalary / 1000)];
+  const nhif = nhifRates[Math.ceil(basicSalary / 10000)];
   const payee = taxBrackets.reduce((acc,bracket) => acc + bracket.rate * Math.max(Math.min(basicSalary - acc, bracket.upperLimit - acc),0),0);
   const nssf = Math.min(basicSalary * 0.06 + benefits * 0.06, 200);
   const grossSalary = basicSalary + benefits;
